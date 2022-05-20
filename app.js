3011f83902c123d83e1e34feb620b90ee6d7a980
programmermohaimin <you@example.com>
2022-05-20 10:48:41 +0600

    ok

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
+//=====First Practice======//
 
+// window.onload = () => {
+//     main();
+// };
+
+// function main() {
+//     const root = document.getElementById('root');
+//     const btn = document.getElementById('change-btn');
+
+//     btn.addEventListener('click', function () {
+//         const bgColor = rgbColor()
+//         root.style.backgroundColor = bgColor
+        
+//     })
+   
+// }
+
+
+
+// function rgbColor() {
+//     const red = Math.floor(Math.random() * 255);
+//     const green = Math.floor(Math.random() * 255);
+//     const blue = Math.floor(Math.random() * 255);
+
+//     return `rgb(${red}, ${green}, ${blue})`;
+// }
+
+
+
+//=======second practice is unseen========//
 window.onload = () => {
-    main();
-};
-
+    main()
+}
 function main() {
     const root = document.getElementById('root');
     const btn = document.getElementById('change-btn');
 
     btn.addEventListener('click', function () {
-        const bgColor = rgbColor()
+        const bgColor = RGB()
         root.style.backgroundColor = bgColor
-        
     })
-   
+
 }
 
 
-
-function rgbColor() {
+function RGB() {
     const red = Math.floor(Math.random() * 255);
     const green = Math.floor(Math.random() * 255);
     const blue = Math.floor(Math.random() * 255);
 
-    return `rgb(${red}, ${green}, ${blue})`;
+    return `rgb(${red}, ${green}, ${blue})`
 }
